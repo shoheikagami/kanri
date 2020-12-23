81ea012360310dd0e03809b7d1ddf4f80ffc7fb8
shoheikagami <shoheikagami@yahoo.co.jp>
2020-12-23 15:33:03 +0900

    initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    $test = 3
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>管理ページ<?= $test; ?></title>
+</head>
+<body>
+    
+</body>
+</html>
